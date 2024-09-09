a610d85717334d08446bf0bf0de58a2b055bb7e4
BuildTools <magidonovan@gmail.com>
2024-09-09 04:52:07 -0400

    Added MP5 and some extra sprites

---
+++ Content/Items/Guns/MP5/MP5SD6.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RangerExpansion.Content.Items.Guns.MP5
+{
+    class MP5SD6 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 60; 
+            Item.height = 26; 
+            Item.scale = 1;
+            Item.rare = ItemRarityID.Green;
+
+            // Use Properties
+            Item.useTime = 5; 
+            Item.useAnimation = 15;
+            Item.reuseDelay = 15;
+            Item.consumeAmmoOnLastShotOnly = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true; 
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 45; ; 
+            Item.knockBack = 3f; 
+            Item.noMelee = true; 
+
+            // Gun Properties
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.shootSpeed = 12f;
+            Item.useAmmo = AmmoID.Bullet;
+        }
+    }
+}
