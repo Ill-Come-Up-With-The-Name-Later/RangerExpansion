baa1c46d32e6984571419a5cc7ec5b9658ce4aac
BuildTools <magidonovan@gmail.com>
2024-09-16 15:55:45 -0400

    Added Piercing reforge to add armor piercing to guns

---
+++ Content/Modifiers/Piercing.cs
@@ -0,0 +1,51 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using System.Collections.Generic;
+using Terraria.Localization;
+
+namespace UltimateRangerExpansion.Content.Modifiers
+{
+    class Piercing : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Ranged;
+        public static LocalizedText ArmorPiercingTooltip { get; private set; }
+
+        private readonly int armorPenetration = 5;
+
+        public override void SetStaticDefaults()
+        {
+            ArmorPiercingTooltip = Mod.GetLocalization($"{LocalizationCategory}.{nameof(ArmorPiercingTooltip)}");
+        }
+
+        public override float RollChance(Item item)
+        {
+            return 3f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 1.105f;
+            shootSpeedMult *= 1.33f;
+        }
+
+        public override void Apply(Item item)
+        {
+            item.ArmorPenetration = armorPenetration;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+            yield return new TooltipLine(Mod, "ArmorPenetration", ArmorPiercingTooltip.Format(armorPenetration)) { IsModifier = true };
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.2f;
+        }
+    }
+}
