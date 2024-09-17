880646649c08589d4c27cfbbc20f4eea5417bb12
BuildTools <magidonovan@gmail.com>
2024-09-17 15:31:12 -0400

    Added Danger as a placeable object

---
+++ Content/Items/Paintings/Danger/DangerPainting.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ModLoader;
+using UltimateRangerExpansion.Content.Rarities;
+using UltimateRangerExpansion.Content.Tiles.Paintings.Danger;
+using Terraria;
+
+namespace UltimateRangerExpansion.Content.Items.Paintings.Danger
+{
+    class DangerPainting : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<DangerPaintingTile>(), 0);
+
+            Item.width = 48;
+            Item.height = 32;
+            Item.maxStack = 9999;
+            Item.rare = ModContent.RarityType<DeveloperRarity>();
+
+            Item.value = Item.buyPrice(1, 0, 0, 0);
+        }
+    }
+}
---
+++ Content/Tiles/Paintings/Danger/DangerPaintingTile.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.DataStructures;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+
+namespace UltimateRangerExpansion.Content.Tiles.Paintings.Danger
+{
+    class DangerPaintingTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileWaterDeath[Type] = false;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            TileObjectData.newTile.CoordinateHeights = [16, 16];
+
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(0, 0, 0), null);
+        }
+    }
+}
