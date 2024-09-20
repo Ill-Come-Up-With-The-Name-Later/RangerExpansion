7edc077a6e624e6354adf79ac15ce0c0374cd71f
BuildTools <magidonovan@gmail.com>
2024-09-20 07:05:45 -0400

    Added special projectile ring to Sunshine

--- Content/Items/Bows/Sunshine/SunshineBow.cs
+++ Content/Items/Bows/Sunshine/SunshineBow.cs
@@ -1,6 +1,9 @@
 ﻿using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace UltimateRangerExpansion.Content.Items.Bows.Sunshine
 {
@@ -48,6 +51,43 @@ namespace UltimateRangerExpansion.Content.Items.Bows.Sunshine
                .AddIngredient(ItemID.Phantasm)
                .AddTile(TileID.LunarCraftingStation)
                .Register();
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 playerPos = player.Center;
+
+            for(int i = 0; i < 8; i ++) // Fire a projectile every 45 degrees from 0 to 360
+            {
+                float angle = i * 45;
+
+                float angleCos = (float)Math.Cos(MathHelper.ToRadians(angle));
+                float angleSin = (float)Math.Sin(MathHelper.ToRadians(angle));
+
+                float projX = playerPos.X + (65 * angleCos);
+                float projY = playerPos.Y - (75 * angleSin);
+
+                Vector2 projPos = new(projX, projY);
+                Projectile projectile = Main.projectile[Projectile.NewProjectile(source, projPos, velocity, type, damage, knockback)];
+                projectile.tileCollide = false;
+
+                for (int k = 0; k < 12; k++) // Draw dust every 30 degrees
+                {
+                    float dustAngle = k * 30;
+
+                    float dustCos = (float)Math.Cos(MathHelper.ToRadians(dustAngle));
+                    float dustSin = (float)Math.Sin(MathHelper.ToRadians(dustAngle));
+
+                    float dustX = projPos.X + (2 * dustCos);
+                    float dustY = projPos.Y - (2 * dustSin);
+
+                    Vector2 dustLoc = new(dustX, dustY);
+                    Dust.NewDust(dustLoc, 1, 1, DustID.SolarFlare, -projectile.velocity.X / 3, -projectile.velocity.Y / 3);
+                    Lighting.AddLight(dustLoc, TorchID.Orange);
+                }
+            }
+
+            return false;
         }
     }
 }
