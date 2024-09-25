7228ceecd71090f10d80d353de604d87954728ec
BuildTools <magidonovan@gmail.com>
2024-09-25 05:34:29 -0400

    Apex mode switching works

--- Content/Items/Guns/Apex/Apex.cs
+++ Content/Items/Guns/Apex/Apex.cs
@@ -3,21 +3,20 @@ using Terraria.ModLoader;
 using Terraria;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using System;
+using Terraria.Audio;
+using Terraria.DataStructures;
 
 namespace UltimateRangerExpansion.Content.Items.Guns.Apex
 {
     class Apex : ModItem
     {
         private int mode = 1;
-        private readonly List<string> modeDesc = [];
+        private readonly List<string> modeDesc = ["Burst Fire", "Shotgun", "Single Shot"];
 
         public override void SetStaticDefaults()
         {
             Item.ResearchUnlockCount = 1;
-
-            modeDesc.Add("Burst Fire");
-            modeDesc.Add("Shotgun");
-            modeDesc.Add("Single Shot");
         }
 
         public override void SetDefaults()
@@ -46,25 +45,86 @@ namespace UltimateRangerExpansion.Content.Items.Guns.Apex
             Item.shootSpeed = 27f;
             Item.useAmmo = AmmoID.Bullet;
 
-            Item.value = Item.buyPrice(0, 15, 20, 50);
+            Item.value = Item.buyPrice(1, 25, 60, 40);
         }
 
         public override bool AltFunctionUse(Player player)
         {
-            if(mode++ > 3)
+            mode++;
+
+            if (mode > 3)
             {
-                mode = 0;
+                mode = 1;
+            }
+
+            switch (mode)
+            {
+                case 1:
+                    Item.useTime = 2;
+                    Item.useAnimation = 6;
+                    Item.reuseDelay = 6;
+                    Item.consumeAmmoOnLastShotOnly = true;
+                    break;
+                case 2:
+                    Item.useTime = 2;
+                    Item.useAnimation = 2;
+                    Item.reuseDelay = 2;
+                    Item.consumeAmmoOnLastShotOnly = false;
+                    break;
+                case 3:
+                    Item.useTime = 2;
+                    Item.useAnimation = 2;
+                    Item.reuseDelay = 2;
+                    Item.consumeAmmoOnLastShotOnly = false;
+                    break;
+            }
+
+            return false;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if(mode == 1)
+            {
+                // Rotate projectile randomy
+                float rotation = MathHelper.ToRadians(5);
+                velocity = velocity.RotatedByRandom(MathHelper.Lerp(-rotation, rotation, 1));
+            }
+
+            SoundEngine.PlaySound(SoundID.Item11, position); // Play sound every shot
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if(mode == 2)
+            {
+                float numberProjectiles = 5 + Main.rand.Next(3);
+                float rotation = MathHelper.ToRadians(20);
+
+                position += Vector2.Normalize(velocity) * 20f;
+                velocity *= 0.33f;
+
+                for (int i = 0; i < numberProjectiles; i++)
+                {
+                    Vector2 perturbedSpeed = velocity.RotatedByRandom(MathHelper.Lerp(-rotation, rotation, 1));
+                    Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+                }
+
                 return false;
             }
 
-            mode++;
+            return true;
+        }
 
-            return base.AltFunctionUse(player);
+        public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            return new Random().Next(0, 100) >= 80;
         }
 
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
             tooltips.Add(new TooltipLine(Mod, "mode", $"Mode: {mode} ({modeDesc[mode - 1]})") { OverrideColor = Color.Green });
+            tooltips.Add(new TooltipLine(Mod, "ammo", "80% chance to save ammo"));
         }
     }
 }
