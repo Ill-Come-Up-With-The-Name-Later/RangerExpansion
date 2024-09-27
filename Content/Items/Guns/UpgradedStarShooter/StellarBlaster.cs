b24deef8f6c20043f7edb3b6fcdfa873dd2cdcaf
BuildTools <magidonovan@gmail.com>
2024-09-27 04:21:09 -0400

    Added Stellar Blaster and Vortex Dart Rifle

---
+++ Content/Items/Guns/UpgradedStarShooter/StellarBlaster.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace UltimateRangerExpansion.Content.Items.Guns.UpgradedStarShooter
+{
+    class StellarBlaster : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 56;
+            Item.height = 22;
+            Item.rare = ItemRarityID.Red;
+
+            // Use Properties
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.reuseDelay = 45;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            Item.consumeAmmoOnLastShotOnly = true;
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 750;
+            Item.knockBack = 6f;
+            Item.noMelee = true;
+
+            // Gun Properties
+            Item.shoot = ProjectileID.SuperStar;
+            Item.shootSpeed = 18f;
+            Item.useAmmo = AmmoID.FallenStar;
+
+            Item.value = Item.buyPrice(0, 45, 20, 50);
+        }
+    }
+}
---
+++ Content/Items/Guns/VortexDartGun/VortexDartRifle.cs
@@ -0,0 +1,59 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace UltimateRangerExpansion.Content.Items.Guns.VortexDartGun
+{
+    class VortexDartRifle : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 62;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Red;
+
+            // Use Properties
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            Item.consumeAmmoOnLastShotOnly = true;
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 55;
+            Item.knockBack = 3f;
+            Item.noMelee = true;
+
+            // Gun Properties
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.shootSpeed = 19f;
+            Item.useAmmo = AmmoID.Dart;
+
+            Item.value = Item.buyPrice(0, 15, 20, 50);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            float numberProjectiles = 2;
+			float rotation = MathHelper.ToRadians(15);
+
+			position += Vector2.Normalize(velocity) * 15f;
+			velocity *= 0.5f; 
+
+			for (int i = 0; i < numberProjectiles; i++) {
+				Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1)));
+                Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+            }
+
+			return false;
+		}
+    }
+}
