a5391896eaf0a1d75d7c1f7f166211bd00bdb649
BuildTools <magidonovan@gmail.com>
2024-10-02 05:37:14 -0400

    Work on Ballistic Missile started

---
+++ Content/Items/MiscWeapons/MissileLauncher/MissileLauncher.cs
@@ -0,0 +1,90 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using UltimateRangerExpansion.Content.Projectiles.Rockets.BallisticMissile;
+using System;
+
+namespace UltimateRangerExpansion.Content.Items.MiscWeapons.MissileLauncher
+{
+    class MissileLauncher : ModItem
+    {
+        Vector2 target = Vector2.Zero;
+        readonly float shootSpeed = 30;
+        readonly float gravity = 10;
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 52;
+            Item.height = 22;
+            Item.rare = ItemRarityID.Red;
+
+            // Use Properties
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            Item.UseSound = SoundID.Item40;
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 25000;
+            Item.knockBack = 8f;
+            Item.noMelee = true;
+            Item.crit = 12;
+
+            // Gun Properties
+            Item.shoot = ModContent.ProjectileType<BallisticMissile>();
+            Item.shootSpeed = shootSpeed;
+            Item.useAmmo = AmmoID.Rocket;
+
+            Item.value = Item.buyPrice(0, 55, 10, 50);
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            target = Main.MouseWorld;
+            Main.NewText($"Set missile target to ({target.X}, {target.Y})");
+
+            return false;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            tooltips.Add(new TooltipLine(Mod, "Target", $"Target: (X: {target.X} | Y: {target.Y})") { OverrideColor = Color.Yellow });
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            float discriminant = (float) 
+                (Math.Pow(shootSpeed, 4) 
+                - gravity * ((gravity * target.X * target.X)
+                + (2 * target.Y * shootSpeed * shootSpeed)));
+
+            if(discriminant < 0)
+                return;
+
+            float root = (float)Math.Sqrt(discriminant);
+            float angle = (shootSpeed * shootSpeed - root) / (gravity * target.X);
+
+            if (player.direction == -1)
+                angle = 180 - angle;
+
+            velocity = new(1, 0);
+
+            if (player.direction == -1)
+                velocity *= -1;
+            
+            velocity.RotatedBy(angle);
+            velocity.Normalize();
+            velocity *= shootSpeed;
+        }
+    }
+}
---
+++ Content/Projectiles/Rockets/BallisticMissile/BallisticMissile.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UltimateRangerExpansion.Content.Projectiles.Rockets.BallisticMissile
+{
+    class BallisticMissile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.IsARocketThatDealsDoubleDamageToPrimaryEnemy[Type] = true;
+            ProjectileID.Sets.PlayerHurtDamageIgnoresDifficultyScaling[Type] = true;
+
+            ProjectileID.Sets.Explosive[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 20;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Ranged;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity.Y -= 1 / 6;
+
+            if (Projectile.owner == Main.myPlayer && Projectile.timeLeft <= 3)
+            {
+                Projectile.PrepareBombToBlow();
+            }
+            else
+            {
+                if (Math.Abs(Projectile.velocity.X) >= 8f || Math.Abs(Projectile.velocity.Y) >= 8f)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        float posOffsetX = 0f;
+                        float posOffsetY = 0f;
+                        if (i == 1)
+                        {
+                            posOffsetX = Projectile.velocity.X * 0.5f;
+                            posOffsetY = Projectile.velocity.Y * 0.5f;
+                        }
+
+
+                        Dust fireDust = Dust.NewDustDirect(new Vector2(Projectile.position.X + 3f + posOffsetX, Projectile.position.Y + 3f + posOffsetY) - Projectile.velocity * 0.5f,
+                            Projectile.width - 8, Projectile.height - 8, DustID.Torch, 0f, 0f, 100);
+                        fireDust.scale *= 2f + Main.rand.Next(10) * 0.1f;
+                        fireDust.velocity *= 0.2f;
+                        fireDust.noGravity = true;
+
+                        Dust smokeDust = Dust.NewDustDirect(new Vector2(Projectile.position.X + 3f + posOffsetX, Projectile.position.Y + 3f + posOffsetY) - Projectile.velocity * 0.5f, Projectile.width - 8, Projectile.height - 8, DustID.Smoke, 0f, 0f, 100, default, 0.5f);
+                        smokeDust.fadeIn = 1f + Main.rand.Next(5) * 0.1f;
+                        smokeDust.velocity *= 0.05f;
+                    }
+                }
+
+                if (Math.Abs(Projectile.velocity.X) <= 15f && Math.Abs(Projectile.velocity.Y) <= 15f)
+                {
+                    Projectile.velocity *= 1.1f;
+                }
+
+                if (Projectile.velocity != Vector2.Zero)
+                {
+                    Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + MathHelper.PiOver2;
+                }
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.velocity *= 0f;
+            Projectile.timeLeft = 3;
+            return false;
+        }
+
+        public override void PrepareBombToBlow()
+        {
+            Projectile.tileCollide = false;
+            Projectile.alpha = 255;
+
+            Projectile.Resize(300, 300);
+            Projectile.knockBack = 8f;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+
+            Projectile.Resize(75, 75);
+
+            for (int i = 0; i < 30; i++)
+            {
+                Dust smokeDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 1.5f);
+                smokeDust.velocity *= 1.4f;
+            }
+
+            for (int j = 0; j < 20; j++)
+            {
+                Dust fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3.5f);
+                fireDust.noGravity = true;
+                fireDust.velocity *= 7f;
+                fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 1.5f);
+                fireDust.velocity *= 3f;
+            }
+
+            for (int k = 0; k < 2; k++)
+            {
+                float speedMulti = 0.4f;
+                if (k == 1)
+                {
+                    speedMulti = 0.8f;
+                }
+
+                Gore smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity += Vector2.One;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity.X -= 1f;
+                smokeGore.velocity.Y += 1f;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity.X += 1f;
+                smokeGore.velocity.Y -= 1f;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity -= Vector2.One;
+            }
+        }
+    }
+}
