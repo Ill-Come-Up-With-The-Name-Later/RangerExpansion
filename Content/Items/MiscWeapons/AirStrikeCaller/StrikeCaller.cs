7eeba50542a9dbb2b1d4d0de52b74b1a20a17925
BuildTools <magidonovan@gmail.com>
2024-10-08 04:24:16 -0400

    Added Air Strike Caller

---
+++ Content/Items/MiscWeapons/AirStrikeCaller/StrikeCaller.cs
@@ -0,0 +1,67 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using UltimateRangerExpansion.Content.Projectiles.AirStrikePlane;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+
+namespace UltimateRangerExpansion.Content.Items.MiscWeapons.AirStrikeCaller
+{
+    class StrikeCaller : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Cyan;
+
+            // Use Properties
+            Item.useTime = 180;
+            Item.useAnimation = 180;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.autoReuse = true;
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 200;
+            Item.noMelee = true;
+            Item.crit = 7;
+
+            // Gun Properties
+            Item.shoot = ModContent.ProjectileType<StrikePlane>();
+            Item.shootSpeed = 15;
+
+            Item.value = Item.buyPrice(0, 25, 0, 0);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = Main.screenPosition;
+            position.X -= 100;
+            position.Y -= 50;
+
+            velocity = new Vector2(Item.shootSpeed, 0);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.PDA)
+                .AddIngredient(ItemID.FlareGun)
+                .AddIngredient(ItemID.SoulofFlight, 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/AirStrikePlane/StrikePlane.cs
@@ -0,0 +1,56 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace UltimateRangerExpansion.Content.Projectiles.AirStrikePlane
+{
+    class StrikePlane : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 58;
+            Projectile.height = 26;
+
+            Projectile.timeLeft = 180;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            if (Projectile.timeLeft % 30 == 0)
+            {
+                if (Projectile.owner == Main.myPlayer)
+                {
+                    Vector2 velocity = new(Projectile.velocity.X / 10, 2);
+
+                    Projectile proj = Main.projectile[Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center,
+                            velocity, ProjectileID.RocketI, 200, 3, Main.myPlayer)];
+
+                    proj.netUpdate = true;
+                    proj.usesLocalNPCImmunity = true;
+                }
+
+                int frameSpeed = 5;
+                Projectile.frameCounter++;
+
+                if (Projectile.frameCounter >= frameSpeed)
+                {
+                    Projectile.frameCounter = 0;
+                    Projectile.frame++;
+
+                    if (Projectile.frame >= Main.projFrames[Projectile.type])
+                    {
+                        Projectile.frame = 0;
+                    }
+                }
+            }
+        }
+    }
+}
