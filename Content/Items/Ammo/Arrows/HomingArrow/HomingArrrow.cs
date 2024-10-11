b05f4bb026977769bfd8fa26af63a72ef2f5e830
BuildTools <magidonovan@gmail.com>
2024-10-11 10:07:18 -0400

    Added code for homing arrows

---
+++ Content/Items/Ammo/Arrows/HomingArrow/HomingArrrow.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using UltimateRangerExpansion.Content.Projectiles.Arrows.HomingArrow;
+
+namespace UltimateRangerExpansion.Content.Items.Ammo.Arrows.HomingArrow
+{
+    class HomingArrrow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.WoodenArrow);
+
+            Item.damage = 8;
+            Item.crit = 3;
+
+            Item.rare = ItemRarityID.LightPurple;
+
+            Item.shoot = ModContent.ProjectileType<HomingArrowProjectile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(150)
+                .AddIngredient(ItemID.WoodenArrow, 150)
+                .AddIngredient(ItemID.SoulofSight, 5)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Arrows/HomingArrow/HomingArrowProjectile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using UltimateRangerExpansion.Utils;
+
+namespace UltimateRangerExpansion.Content.Projectiles.Arrows.HomingArrow
+{
+    class HomingArrowProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.WoodenArrowFriendly);
+            Projectile.timeLeft = 2400;
+        }
+
+        public override void AI()
+        {
+            Player owner = Main.player[Projectile.owner];
+
+            Projectile.rotation += MathHelper.Pi;
+
+            NPC npc = Utilities.ClosestNPC(Projectile.position, 900);
+
+            if (npc != null)
+            {
+                Vector2 npcPos = npc.position;
+                Vector2 velocity = npcPos - Projectile.position;
+                velocity.Normalize();
+                velocity *= 15;
+
+                Projectile.velocity = velocity;
+            }
+        }
+    }
+}
